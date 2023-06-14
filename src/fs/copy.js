19355efb08f391b153f90b60bd0fb67988806eca
Zara2ctra <116357056+Zara2ctra@users.noreply.github.com>
2023-06-14 17:39:14 +0400

    feat: add copy file task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
+import { existsSync, readdir, mkdir, copyFile } from 'fs';
+
 const copy = async () => {
-    // Write your code here 
+    if (!existsSync('src/fs/files/') || existsSync('src/fs/files_copy/')) {
+        throw Error('FS operation failed')
+    } else {
+        readdir('src/fs/files/', 'utf8', (err, files) => {
+            if (err) throw err;
+            mkdir('src/fs/files_copy', (err) => {
+                if (err) throw err;
+            });
+            for (let i = 0; i < files.length; i++) {
+                copyFile(`src/fs/files/${files[i]}`, `src/fs/files_copy/${files[i]}`, (err) => {
+                    if (err) throw err;
+                })
+            }
+
+
+        })
+    }
 };
 
 await copy();
