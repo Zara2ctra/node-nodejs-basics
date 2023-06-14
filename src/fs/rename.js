8bae21da4a8ddeba07b2114d17cf3224c6fd7e98
Zara2ctra <116357056+Zara2ctra@users.noreply.github.com>
2023-06-14 17:51:47 +0400

    feat: add rename file task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,13 @@
+import fs from "fs";
+
 const rename = async () => {
-    // Write your code here 
+    if (!fs.existsSync('src/fs/files/wrongFilename.txt') || fs.existsSync('src/fs/files/properFilename.md')) {
+        throw Error('FS operation failed')
+    } else {
+        fs.rename('src/fs/files/wrongFilename.txt', 'src/fs/files/properFilename.md', err => {
+            if (err) throw err;
+        })
+    }
 };
 
 await rename();
