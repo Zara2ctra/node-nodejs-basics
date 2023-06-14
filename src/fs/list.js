c76fede05b6cd8cedf35d67789a2c1374ee5539c
Zara2ctra <116357056+Zara2ctra@users.noreply.github.com>
2023-06-14 18:44:20 +0400

    feat: add list file task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+import {existsSync, readdir} from "fs";
+
+
 const list = async () => {
-    // Write your code here 
+    if (!existsSync('src/fs/files/')) {
+        throw Error('FS operation failed')
+    } else {
+        readdir('src/fs/files/', 'utf8', (err, files) => {
+            console.log(files)
+            if (err) throw err;
+        })
+    }
 };
 
 await list();
