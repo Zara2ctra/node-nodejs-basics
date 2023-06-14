2e1f27d280727022a8d6ab1e7f072304a96fc60b
Zara2ctra <116357056+Zara2ctra@users.noreply.github.com>
2023-06-14 18:50:49 +0400

    feat: add read file task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,10 @@
+import { readFile } from 'fs';
+
 const read = async () => {
-    // Write your code here 
+    readFile('src/fs/files/fileToRead.txt', 'utf8', (err, fileContent) => {
+        if (err) throw Error('FS operation failed')
+        console.log(fileContent);
+    })
 };
 
 await read();
