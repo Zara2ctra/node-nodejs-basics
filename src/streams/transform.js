b5538d12fb5b2def5748651d65562dc1c333737c
Zara2ctra <116357056+Zara2ctra@users.noreply.github.com>
2023-06-16 03:34:34 +0400

    fix: refactoring code

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,7 +1,6 @@
 import { stdin, stdout } from 'node:process';
 
 const transform = async () => {
-
         stdin.on('data', data => {
             data = data.toString().split('').reverse().join('').trim()
             stdout.write(data + "\n\n")
