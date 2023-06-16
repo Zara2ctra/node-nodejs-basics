0e0360f1b970af1efca0c3e14332afd0aa1ae2fe
Zara2ctra <116357056+Zara2ctra@users.noreply.github.com>
2023-06-16 19:39:13 +0400

    feat: complete unzip task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,14 @@
+import zlib from "zlib";
+import {createReadStream, createWriteStream} from "node:fs";
+
 const decompress = async () => {
-    // Write your code here 
+    let unzip = zlib.createUnzip();
+    let readStream = createReadStream('src/zip/files/archive.gz');
+    let writeStream = createWriteStream('src/zip/files/fileToCompress.txt');
+
+    readStream
+        .pipe(unzip)
+        .pipe(writeStream);
 };
 
 await decompress();
